abf8cb93d8801ee86e7d4b234b984bd046169a2c
vikmik80 <100407222+vikmik80@users.noreply.github.com>
2022-02-25 11:31:25 +0300

    Add files via upload

---
+++ project1/src/main/java/by/mikshuta/task1/entity/Array.java
@@ -0,0 +1,12 @@
+package main.java.by.mikshuta.task1.entity;
+
+public class Array {
+
+	public static void main(String[] args) {
+		var a = 2;
+		var b = 3;
+		System.out.println("a + b = " + (int)(a + b));
+
+	}
+
+}
